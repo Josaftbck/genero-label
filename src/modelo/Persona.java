332c76aca690966b2f5c0150c49292fd543eec2c
Josaftbck <josaftbck@gmail.com>
2024-09-28 09:15:27 -0600

    genero mujer/hombre

---
+++ src/modelo/Persona.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author LG
+ */
+
+// constructor
+abstract class Persona {
+    private String nombres,apellidos,direccion,telefono,fecha_nacimiento;
+    private boolean genero;
+    
+    public Persona(){}
+    
+    public Persona(String nombres, String apellidos, String direccion, String telefono, String fecha_nacimiento, boolean genero) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.genero = genero;
+    }
+
+    
+    // getter and setters
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(String fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+    
+    public boolean getGenero(){
+        return genero;
+    } 
+    
+    public void setGenero(boolean genero){
+        this.genero = genero;
+    }
+    
+    protected void crear(){}
+    protected DefaultTableModel leer(){ return null;}
+    protected void actualizar(){};
+    protected void borrar(){};
+    
+}
